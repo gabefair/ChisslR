dbc687379f005e078aab8148562f1d2546f81eb0
Gabriel Fair <gabrielfair@gmail.com>
2019-12-05 20:27:20 -0500

    update download_mnist_data function

--- helpers/data-prep.R
+++ helpers/data-prep.R
@@ -2,8 +2,9 @@ library(readr)
 library(dplyr)
 
 download_mnist_data <- function() {
-  if (FALSE %in% (list.files(path="../data/") == 'mnist_raw.csv')){
-    mnist_raw <- read_csv("https://pjreddie.com/media/files/mnist_train.csv", col_names = FALSE)
-    write_csv(mnist_raw,"../data/mnist_raw.csv", row.names = FALSE)
+  if (!file.exists('data/mnist_raw.csv')){
+    #mnist_raw <- read_csv("https://pjreddie.com/media/files/mnist_train.csv", col_names = FALSE)
+    write_csv(read_csv("https://pjreddie.com/media/files/mnist_train.csv", col_names = FALSE),"data/mnist_raw.csv")
   }
-}
+  else {print("mnist_raw.csv already downloaded")}
+} #This function might seem to freze for one or two mins. Give it time
