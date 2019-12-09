f2efe6881bff5b86b2fabc251dc5fc24363c8e38
Gabriel Fair <gabrielfair@gmail.com>
2019-12-09 13:44:25 -0500

    add newsgroups data function

--- helpers/data-prep.R
+++ helpers/data-prep.R
@@ -12,7 +12,18 @@ download_mnist_data <- function() {
 
 download_chissl_mongodb <- function() {
   if (!file.exists('data/chissl.agz')){
-    download.file("https://github.com/pnnl/chissl/raw/e20193546b2a81f4906fa5316868181e527e1fc2/dumps/chissl",destfile="data/chissl.agz",method="libcurl")
+    download.file("https://github.com/pnnl/chissl/raw/e20193546b2a81f4906fa5316868181e527e1fc2/dumps/chissl", destfile="data/chissl.agz", method="libcurl")
   }
   else {print("chissl.agz already downloaded")}
+}
+
+download_newsgroups_byDate <- function() {
+  if (!file.exists('data/20news-bydate.tar.gz')){
+    download.file("http://qwone.com/~jason/20Newsgroups/20news-bydate.tar.gz", destfile = "data/20news-bydate.tar.gz", method="libcurl")
+  }
+  else {print("20news-bydate.tar.gz already downloaded")}
+}
+
+extract_newsgroups_byDate <- function() {
+  
 }
